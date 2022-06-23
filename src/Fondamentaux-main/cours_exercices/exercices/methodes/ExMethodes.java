006210c28403aa68f4e99c6ba15c0d3f823918ca
VictorDauph <victor.dauph@gmail.com>
2022-06-23 11:31:39 +0200

    exos methodes

--- src/Fondamentaux-main/cours_exercices/exercices/methodes/ExMethodes.java
+++ src/Fondamentaux-main/cours_exercices/exercices/methodes/ExMethodes.java
@@ -13,10 +13,39 @@
 package cours_exercices.exercices.methodes;
 
 // Classe principale
+
+public class ExMethodes {
     // Méthode maximum
+    public int Max(int a,int b,int c){
+        if(a>b && a>c){
+           return a;
+        }
+        else if(b>a && b>c){
+            return b;
+        }
+        else if(c>a && c>b){
+            return c;
+        }else{
+        return -1;
+        }
+    }
     
     // Méthode cube
-    
+    public int cube(int val){
+        return val*val*val;
+    }
+
     // Méthode volume
-    
+    public double volume(int r){
+        return 4/3*3.14*cube(r);
+    }
+
     // Méthode principale
+    public static void main(String[] args) {
+        ExMethodes m = new ExMethodes();
+        
+        System.out.println(m.Max(1,2,2));
+        System.out.println(m.cube(2));
+        System.out.println(m.volume(3));
+    }
+}
