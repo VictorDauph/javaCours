cd2a27db9ee2a338dab6c2553df4ba9561116978
VictorDauph <victor.dauph@gmail.com>
2022-06-23 11:09:49 +0200

    exemples methodes

--- src/Fondamentaux-main/cours_exercices/cours/Methodes.java
+++ src/Fondamentaux-main/cours_exercices/cours/Methodes.java
@@ -7,12 +7,37 @@ package cours_exercices.cours;
 // Classe principale
 public class Methodes {
     // Méthode sans paramètre et ne retournant pas de valeur.
+    public void ecrit(){
+        System.out.println("la methode erit");
+    }
 
     // Méthode avec un paramètre et ne retournant pas de valeur.
+    public void ecrit2(String monTexte){
+        System.out.println(monTexte);
+    }
 
     // Méthode avec plusieurs paramètres et ne retournant pas de valeur.
+    public void ecritParam2(String monTexte,String monAutreTexte){
+        System.out.println(monTexte);
+        System.out.println(monAutreTexte);
+    }
 
     // Méthode avec paramètre et retournant une valeur.
+    public int carre(int valeur){
+        return valeur*valeur;
+        
+    }
     
     // Méthode principale
+    //public static est le point d'entrée de la classe "Méthodes".
+    public static void main(String[] args){
+        
+        //Cette ligne permet d'appeler dans la méthode principale
+        Methodes m = new Methodes();
+        
+        m.ecrit();
+        m.ecrit2("blablabla");
+        System.out.println(m.carre(2));
+    }
+    
 }
