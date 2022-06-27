49e0d4793cb843d0c8fdc68498527f0fed0967b0
VictorDauph <victor.dauph@gmail.com>
2022-06-27 12:17:35 +0200

    cours interfaces dérivées

--- src/Fondamentaux-main/cours_exercices/cours/InterfacesDerivees.java
+++ src/Fondamentaux-main/cours_exercices/cours/InterfacesDerivees.java
@@ -5,27 +5,58 @@
 package cours_exercices.cours;
 
 // Interface avec méthode défaut
-
+interface IAffichage{
+    default void affiche(){
+        System.out.println("Je suis un Iaffichage ");
+    }
+}
 // Interface dérivée de l'interface IAffichage,
 // utilise la méthode par défaut de IAffichage.
-
+interface IAffichageDerivee1 extends IAffichage{
+    
+}
 // Interface dérivée de l'interface IAffichage,
 // la méthode affiche devient abstraite.
+interface IAffichageDerivee2 extends IAffichage{
+    @Override void affiche(); //rends la méthode abstraite
+}
 
 // Interface dérivée de l'interface IAffichage,
 // redéfinie la méthode affiche de IAffichage.
+interface IAffichageDerivee3 extends IAffichage{
+    @Override default void affiche(){
+        System.out.println("Je suis IAffichageDerivee3 ");
+    }
+}
 
 // Classe A implémentant IAffichageDerivee1 qui implémente IAffichage.
 // Pas besoin d'être définie grâce à la méthode default.
-
+class A implements IAffichageDerivee1{
+    
+}
 // Classe B implémentant IAffichageDerivee2 qui implémente IAffichage.
 // Affiche doit être définie, car elle est devenue abstraite.
+class B implements IAffichageDerivee2{
+    @Override public void affiche(){
+        System.out.println("je suis un B");
+    }
+}
 
 // Classe C implémentant IAffichageDerivee1 qui implémente IAffichage.
 // Pas besoin d'être définie grâce à la méthode default.
+class C implements IAffichageDerivee3{
+    
+}
 
 // Classe principale
 public class InterfacesDerivees {
     public static void main(String[] args) {
+        A a = new A();
+        B b = new B();
+        C c = new C();
+        
+        a.affiche();
+        b.affiche();
+        c.affiche();
     }
 }
