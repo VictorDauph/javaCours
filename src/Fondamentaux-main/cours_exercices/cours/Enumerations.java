16e27bb9aec56082cbdea14ac1b073f51f76f1aa
VictorDauph <victor.dauph@gmail.com>
2022-06-27 15:21:56 +0200

    cours enumerations

--- src/Fondamentaux-main/cours_exercices/cours/Enumerations.java
+++ src/Fondamentaux-main/cours_exercices/cours/Enumerations.java
@@ -7,17 +7,52 @@ package cours_exercices.cours;
 // Enumération
 // Avec enum on ne crée pas autant de constante qu'il y a de jours.
 // On crée un ensemble de constantes affectées à un seul nom.
+enum Jours{
+    LUNDI,
+    MARDI,
+    MERCREDI
+}
 
 // Classe sans enumérations
+class SansEnum{
     // Attributs
-
+    private static final int LUNDI=1;
+    private static final int MARDI=2;
+    private static final int MERCREDI=3;
+    
     // Méthode
+    public void methodeTest(int maDonnee){
+        if (maDonnee == LUNDI){
+            System.out.println(LUNDI);
+        }
+        if (maDonnee == MARDI){
+            System.out.println(LUNDI);
+        }
+        if (maDonnee == MERCREDI){
+            System.out.println(MERCREDI);
+        }
+    }
+}
+
 
 // Classe principale
 public class Enumerations {
     // Méthode
+    public void methodeTest(Jours quelJour){
+        switch (quelJour){
+            case LUNDI: System.out.println(Jours.LUNDI);break;
+            case MARDI: System.out.println(Jours.MARDI);break;
+            case MERCREDI: System.out.println(Jours.MERCREDI);break;
+            default: System.out.println("Autre jour");break;
+        }
+    }
 
     // Méthode principale
     public static void main(String[] args) {
+        SansEnum se = new SansEnum();
+        Enumerations e = new Enumerations();
+        
+        se.methodeTest(1);
+        e.methodeTest(Jours.MARDI);
     }
 }
